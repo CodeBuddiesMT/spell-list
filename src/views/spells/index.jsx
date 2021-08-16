68f7ee828375606655f7690ad89736a7a8e1543f
Mathew Gutierrez <mathewgutierrez00@gmail.com>
2021-08-15 22:51:02 -0600

    add request for spells

--- src/views/spells/index.jsx
+++ src/views/spells/index.jsx
@@ -1,10 +1,20 @@
 import styled from "styled-components";
+
+const request = require("request");
 const Container = styled.div`
   width: 100%;
   height: 100%;
   background-color: #6d6d6d;
 `;
 const Spells = (props) => {
+  // obviously uses local host right now. Will want to update this with an .env variable when we deploy
+  request("http://localhost:5000/spells", { json: true }, (err, res, body) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(body);
+  });
+
   return <Container>Spells!</Container>;
 };
 
