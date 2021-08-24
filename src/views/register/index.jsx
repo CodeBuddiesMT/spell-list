2f8c2d386679e0f18e7c94d52073e663f0af4c90
tsbarrett89 <tsbarrett89@gmail.com>
2021-08-24 11:52:57 -0600

    Container styled div added

--- src/views/register/index.jsx
+++ src/views/register/index.jsx
@@ -1,7 +1,15 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: #6d6d6d;
+`;
+
 const register = () => {
 
     return (
-        <div></div>
+        <Container>Register!</Container>
     )
 }
 
