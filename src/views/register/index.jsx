097c8e29a646f7f122cb3cb94ffd8b6e75475da5
tsbarrett89 <tsbarrett89@gmail.com>
2021-08-24 15:37:33 -0600

    basic TextFields added

--- src/views/register/index.jsx
+++ src/views/register/index.jsx
@@ -1,10 +1,21 @@
+import { useState } from 'react';
 import styled from "styled-components";
 import { useForm } from 'react-hook-form';
+import { TextField } from '@material-ui/core';
 
 const Container = styled.div`
   width: 100%;
   height: 100%;
   background-color: #6d6d6d;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  form {
+      background-color: gray;
+      display: flex;
+      flex-direction: column;
+  }
 `;
 
 const Registration = () => {
@@ -12,7 +23,16 @@ const Registration = () => {
     const [errorStatus, setErrorStatus] = useState(null);
 
     return (
-        <Container>Register!</Container>
+        <Container>
+            <form>
+                <TextField>
+                    Username
+                </TextField>
+                <TextField>
+                    Password
+                </TextField>
+            </form>
+        </Container>
     )
 }
 
