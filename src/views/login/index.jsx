6a86e58ccb8b78010e2e39521e784cf265e61b6c
tsbarrett89 <tsbarrett89@gmail.com>
2021-08-25 11:46:56 -0600

    setup

--- src/views/login/index.jsx
+++ src/views/login/index.jsx
@@ -1,12 +1,28 @@
+import { useState } from 'react';
 import styled from "styled-components";
+import { Button, TextField } from '@material-ui/core';
 
 const Container = styled.div`
   width: 100%;
   height: 100%;
   background-color: #6d6d6d;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  form {
+      background-color: white;
+      display: flex;
+      flex-direction: column;
+      padding: 1%;
+
+      .registration-input {
+          margin: 2%;
+      }
+  }
 `;
 
-const Login = (props) => {
+const Login = () => {
   return <Container>Login!</Container>;
 };
 
